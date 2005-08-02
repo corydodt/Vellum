3c1b69941243deef67d98326f20c9872f78d497d
corydodt <corydodt>
2005-08-02 17:50:42 +0000

    r213: use the mind argument to create a client list
    committer: corydodt <corydodt>

--- code/vellum/gui/net.py
+++ code/vellum/gui/net.py
@@ -39,6 +39,14 @@ class NetModel(model.Model):
 class NetClient(SilentController):
     _prop_pattern = re.compile(r'property_(?P<prop>.*)_change_notification', )
 
+    def __init__(self, netmodel):
+        self.pbfactory = pb.PBClientFactory()
+        self.remote_control = None
+        self.map = None
+
+        netmodel.registerObserver(self)
+        self.netmodel = netmodel
+
     def __getattr__(self, name):
         """Route changes to the map through my MapView"""
         m = self._prop_pattern.match(name)
@@ -48,48 +56,44 @@ class NetClient(SilentController):
         raise AttributeError(name)
 
     def updateListener(self, propname, model, value):
-        if self.remote_listener is not None:
-            d = self.remote_listener.callRemote("%s_event" % (propname,), 
-                                                model.id, value)
+        if self.remote_control is not None:
+            d = self.remote_control.callRemote("%s_event" % (propname,), 
+                                               model.id, value)
             # TODO - wait for d?
-
-    def __init__(self, netmodel):
-        self.pbfactory = pb.PBClientFactory()
-        self.remote_listener = None
-        self.map = None
-
-        netmodel.registerObserver(self)
-        self.netmodel = netmodel
 
     def property_server_change_notification(self, model, old, new):
         self.connectPB(new, PBPORT)
 
-    def _cb_connected(self, ref):
-        assert hasattr(ref, 'callRemote'), 'Not connected: %s' % (ref,)
-        log.msg('connected %s' % (repr(ref,)))
-        self.remote = ref
-        return ref
+    def property_map_change_notification(self, model, old, new):
+        pass
+
+    def _cb_connected(self, avatar):
+        assert hasattr(avatar, 'callRemote'), 'Not connected: %s' % (avatar,)
+        log.msg('connected %s' % (repr(avatar,)))
+        self.avatar = avatar
+        return avatar
 
 
     def connectPB(self, server, port):
         reactor.connectTCP(server, port, self.pbfactory)
 
+        self.map_view = MapView()
         creds = credentials.UsernamePassword(self.netmodel.username, 'X')
+        d = self.pbfactory.login(creds, self.map_view)
 
-        d = self.pbfactory.login(creds)
         d.addCallback(self._cb_connected)
-        d. addCallback(lambda _: self.remote.callRemote('getInitialMap'))
-        d.  addCallback(self.gotMapData)
-        d.   addCallback(lambda listener: 
-                self.remote.callRemote('iWantUpdates', listener))
-        d.    addCallback(self.receivedListener)
-        d.     addCallback(lambda _: self.collectFiles())
+        d. addCallback(lambda _: self.avatar.callRemote('getInitialMap'))
+        d.  addCallback(self.gotMap)
+        d.   addCallback(lambda _: self.collectFiles())
         d.addErrback(lambda reason: log.err(reason))
         return d
 
-    def gotMapData(self, data):
+    def gotMap(self, (data, remote_control)):
+        self.remote_control = remote_control
+
         map = Map.loadFromYaml(data)
         self.netmodel.map = map
+        self.map_view.map = map
 
         log.msg("Received map, bringing view up to date!!")
         # force updates to the model to take effect immediately
@@ -106,11 +110,6 @@ class NetClient(SilentController):
                 icon.iconcorner = icon.iconcorner
             icon.registerObserver(self)
         # TODO: drawings, notes, sounds
-        return MapView(map)
-
-
-    def receivedListener(self, remote_listener):
-        self.remote_listener = remote_listener
 
     def collectFiles(self):
         return defer.maybeDeferred(self._getNextFile, 
--- code/vellum/server/pb.py
+++ code/vellum/server/pb.py
@@ -18,91 +18,93 @@ from zope import interface
 from vellum.server.map import Map, Icon
 from vellum.server.fs import fs
 
-
 class MapView(pb.Viewable):
     """Make calls on this remote object to push updates across the wire"""
-    def __init__(self, map, ):
-        self.map = map
-
-    def view_mapicon_added_event(self, perspective, oid, new):
+    def __getattr__(self, name):
+        if name.startswith('view_'):
+            if getattr(self, 'map', None) is None:
+                raise RuntimeError("%r: map is not set" % (self,))
+            return getattr(self, name[5:])
+        raise AttributeError(name)
+    def mapicon_added_event(self, perspective, oid, new):
         i = self.map.iconFromId(oid)
         i.__dict__.update(new)
         self.map.icon_added = i
         log.msg("icon added")
-    def view_mapicon_removed_event(self, perspective, oid, new):
+    def mapicon_removed_event(self, perspective, oid, new):
         i = self.map.iconFromId(oid)
         assert i is not None, 'No icon with id %s found' % (oid,)
         self.map.removeIcon(i)
         self.map.icon_removed = i
         log.msg("icon removed")
-    def view_iconname_event(self, perspective, oid, new):
+    def iconname_event(self, perspective, oid, new):
         i = self.map.iconFromId(oid)
         i.iconname = new
         log.msg("iconname changed")
-    def view_iconuri_event(self, perspective, oid, new):
+    def iconuri_event(self, perspective, oid, new):
         assert None, 'not implemented'
         log.msg("iconimage changed")
-    def view_iconsize_event(self, perspective, oid, new):
+    def iconsize_event(self, perspective, oid, new):
         i = self.map.iconFromId(oid)
         i.iconsize = new
         log.msg("iconsize changed")
-    def view_iconcorner_event(self, perspective, oid, new):
+    def iconcorner_event(self, perspective, oid, new):
         i = self.map.iconFromId(oid)
         i.iconcorner = new
         log.msg("iconcorner changed")
-    def view_lastwindow_event(self, perspective, oid, new):
+    def lastwindow_event(self, perspective, oid, new):
         self.map.lastwindow = new
         log.msg("lastwindow changed")
-    def view_attention_event(self, perspective, oid, new):
+    def attention_event(self, perspective, oid, new):
         self.map.attention = new
         log.msg("attention changed")
-    def view_scale100px_event(self, perspective, oid, new):
+    def scale100px_event(self, perspective, oid, new):
         self.map.scale100px = new
         log.msg("scale100px changed")
-    def view_mapname_event(self, perspective, oid, new):
+    def mapname_event(self, perspective, oid, new):
         self.map.mapname = new
         log.msg("mapname changed")
-    def view_mapuri_event(self, perspective, oid, new):
+    def mapuri_event(self, perspective, oid, new):
         assert None, 'not implemented'
         log.msg("image changed")
-    def view_laser_event(self, perspective, oid, new):
+    def laser_event(self, perspective, oid, new):
         self.map.laser = new
         log.msg("laser changed")
-    def view_obscurement_event(self, perspective, oid, new):
+    def obscurement_event(self, perspective, oid, new):
         assert None, 'not implemented'
         log.msg("obscurement changed")
 
 class Gameboy(pb.Avatar):
-    def __init__(self, name):
+    def __init__(self, name, peers):
         cp = ConfigParser.ConfigParser()
         cp.read(fs.ini)
 
-        self.peers = []     # MapListener refs from clients
+        self.peers = peers
 
         lastmap = cp.get('vellumpb', 'lastmap', None)
         self.map = Map.loadFromYaml(file(lastmap, 'rb').read())
-        self.observable = MapView(self.map, )
+        self.observable = MapView()
+        self.observable.map = self.map
 
     def perspective_getInitialMap(self):
-        return self.map.describeToYaml()
-
-    def perspective_iWantUpdates(self, listener):
-        """Here's an object you can use to tell me about map updates"""
-        self.peers.append(listener)
-        return self.observable
+        return self.map.describeToYaml(), self.observable
 
 
 class GameRealm:
     interface.implements(portal.IRealm)
+    def __init__(self):
+        self.clients = []
+
     def requestAvatar(self, avatarId, mind, *interfaces):
         if pb.IPerspective not in interfaces:
             raise NotImplementedError
-        avatar = Gameboy(avatarId)
-        return pb.IPerspective, avatar, lambda:None
+        avatar = Gameboy(avatarId, self.clients)
+        self.clients.append(mind)
+        return pb.IPerspective, avatar, lambda:self.clients.remove(mind)
 
 c = checkers.InMemoryUsernamePasswordDatabaseDontUse(jezebel='X',
-                                              gm='X',
-                                              )
+                                                     gm='X',
+                                                     )
 gameportal = portal.Portal(GameRealm())
 gameportal.registerChecker(c)
 
