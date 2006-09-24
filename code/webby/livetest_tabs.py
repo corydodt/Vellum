0865ff265168f8bd1b3d0e43fdf166f36dfc0391
corydodt <corydodt>
2006-09-24 17:48:42 +0000

    r333: name the testcase appropriately for tabs
    committer: corydodt <corydodt>

--- code/webby/livetest_tabs.py
+++ code/webby/livetest_tabs.py
@@ -3,7 +3,7 @@ from nevow.livetrial import testcase
 
 from webby import tabs
 
-class TestInitialArguments(testcase.TestCase):
+class TestTabs(testcase.TestCase):
     jsClass = u'Tabby.Tests.TestTabs'
     def newTabWidget(self, *a):
         """Return a new tab widget"""
