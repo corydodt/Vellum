7e4ef050a406254b089b2cd74a67f66a25062920
corydodt <corydodt>
2006-09-27 21:46:01 +0000

    r347: convert tabs to a LiveElement
    committer: corydodt <corydodt>

--- code/webby/tabs.py
+++ code/webby/tabs.py
@@ -2,13 +2,11 @@ from twisted.python.util import sibpath
 
 from nevow import loaders, athena
 
-RESOURCE = lambda f: sibpath(__file__, f)
-
-class TabsFragment(athena.LiveFragment):
+class TabsFragment(athena.LiveElement):
     docFactory = loaders.xmlstr(
 """<span xmlns:n="http://nevow.com/ns/nevow/0.1"
 xmlns:athena="http://divmod.org/ns/athena/0.7"
-n:render="liveFragment"
+n:render="liveElement"
 class="tabsFragment">
     <div class="handles" />
     <div class="panes" />
