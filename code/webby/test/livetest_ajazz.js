3861d335498790f77f60b3dcfcaf49bbbc826237
corydodt <corydodt>
2006-09-30 19:07:10 +0000

    r357: separate 'initialization' aspects of the test into its own method
    committer: corydodt <corydodt>

--- code/webby/test/livetest_ajazz.js
+++ code/webby/test/livetest_ajazz.js
@@ -12,14 +12,36 @@ MockEvent.methods(
 
 WebbyVellum.Tests.TestIRCContainer = Nevow.Athena.Test.TestCase.subclass("WebbyVellum.Tests.TestIRCContainer");
 WebbyVellum.Tests.TestIRCContainer.methods(
+    function test_initialize(self) {
+        var d = self.setUp();
+        d.addCallback(function _(irc) {
+            // check that subwidgets are present and supply the right methods
+            var accountManager = irc.childWidgets[0];
+            self.failIf(accountManager.onLogOnSubmit === undefined);
+            var conv = irc.childWidgets[1];
+            self.failIf(conv.appendToTab === undefined);
+            var chat = irc.childWidgets[2];
+            self.failIf(chat.submit === undefined);
+
+            var chatentry = chat.firstNodeByAttribute('class', 'chatentry');
+
+            // entry field should start empty
+            self.assertEqual(chatentry.value, '');
+
+            var fgtab = irc.firstNodeByAttribute('class', 'tab');
+
+            // the server tab should be in the foreground
+            self.assertEqual(fgtab.id, '**SERVER**');
+
+            // TODO - assert something about accountManager
+        });
+    },
+
     function test_submitText(self) {
         var d = self.setUp();
         d.addCallback(function _(irc) {
             var chat = irc.childWidgets[2];
             var chatentry = chat.firstNodeByAttribute('class', 'chatentry');
-
-            // entry field should start empty
-            self.assertEqual(chatentry.value, '');
 
             chatentry.value = 'hello';
             var event = new MockEvent(chat.node);
@@ -28,11 +50,7 @@ WebbyVellum.Tests.TestIRCContainer.methods(
                 // should reset the entry field to empty
                 self.assertEqual(chatentry.value, '');
 
-                var pw = chat.widgetParent;
-                var fgtab = pw.firstNodeByAttribute('class', 'tab');
-
-                // the server tab should be in the foreground
-                self.assertEqual(fgtab.id, '**SERVER**');
+                var fgtab = irc.firstNodeByAttribute('class', 'tab');
 
                 // the server tab should contain a span with the text
                 self.assertEqual(fgtab.innerHTML.search('hello') > 0, true);
